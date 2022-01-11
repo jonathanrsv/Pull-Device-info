41df165ca761bf7e6a7bf523a7ff1e3ddf81a08b
Jonathan Rodrigues da Silva <CIANDT\jonathanrs@lnb028990spo.ciandt.global>
2022-01-11 15:33:55 -0300

    melhorias

--- pull_device_info.py
+++ pull_device_info.py
@@ -1,6 +1,7 @@
 import csv
 from requests_html import HTMLSession
 from unicodedata import normalize 
+import pprint
 
 def clear_text(txt):
     return normalize('NFKD', txt).encode('ASCII', 'ignore').decode('ASCII')
@@ -12,7 +13,7 @@ def get_phone_list(csv_file):
             for row in spamreader:
                 phone_url = get_url_phone_data("https://www.maiscelular.com.br/pesquisa/?q=" + row[0])
                 if phone_url: 
-                    print("Device processado: " + row[0])
+                    print("Device processado: " + row[0] + ', url: ' + phone_url)
                     normalized.append({
                         'modelo':row[0], 
                         'url_de_busca': phone_url,
@@ -37,22 +38,36 @@ def get_phone_table_data(url):
     phone_data = {}
     try:
         for tr in element:
-            phone_data[clear_text(tr.find("td")[0].text)] = tr.find("td")[1].text
-    except:
-        print("Falha na url de busca: " + url)
+            valor = 'null'
+            if len(tr.find("td")) > 1: 
+                 valor = tr.find("td")[1].text 
+            phone_data[clear_text(tr.find("td")[0].text)] = valor
+    except ValueError:
+        print("Falha na url de busca: " + ValueError)
         pass
     return phone_data
 
 def write_csv(data, csv_name):
     with open(csv_name, mode='w',  encoding='latin', newline='') as file: 
-        writer = csv.writer(file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer = csv.writer(file, delimiter=';')
+        writer.writerow([
+                "modelo",
+                'Marca', 
+                'Memoria RAM',
+                'NFC',
+                'Capacidade bateria'
+        ])
+
         for phone in data:
             phone_info = phone["dados"]
+
             writer.writerow([
                 phone["modelo"],
                 phone_info['Marca'], 
-                phone_info['Modelo'],
-                phone_info['Memoria RAM'].split(" ")[0]
+                #phone_info['Modelo'],
+                phone_info['Memoria RAM'].split(" ")[0],
+                phone_info['NFC'],
+                phone_info['Capacidade bateria']
             ])
 
 def pull_device_info(csv_file, csv_file_output):
---
+++ run.py
@@ -0,0 +1,3 @@
+from pull_device_info import pull_device_info
+
+pull_device_info("devices_list.csv", "output_file.csv")
